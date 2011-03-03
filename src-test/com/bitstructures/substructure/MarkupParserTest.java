2d9353875cbda929a88d9f05c1cad5daca430f41
Simon Bates <simon@bitstructures.com>
2011-03-03 18:18:54 -0500

    Read in the test data with Guava Resources.toString() rather than using an InputStream

--- src-test/com/bitstructures/substructure/MarkupParserTest.java
+++ src-test/com/bitstructures/substructure/MarkupParserTest.java
@@ -1,14 +1,15 @@
 package com.bitstructures.substructure;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.StringReader;
+import java.net.URL;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
 
 public class MarkupParserTest extends Assert {
 	
@@ -27,49 +28,28 @@ public class MarkupParserTest extends Assert {
 		int expectedNumberOfTestCases = 3;
 		
 		int n = 1;
-		Reader markupReader = getReader(MARKUP_FILE_NAME_TEMPLATE, n);
-		while (markupReader != null) {
-			try {
+		boolean moreToDo = true;
+		while (moreToDo) {
+			String markup = getContents(MARKUP_FILE_NAME_TEMPLATE, n);
+			if (markup == null) {
+				moreToDo = false;
+			} else {
+				StringReader markupReader = new StringReader(markup);
 				String expected = getContents(EXPECTED_FILE_NAME_TEMPLATE, n);
 				assertEquals(expected, markupParser.parse(markupReader));
-			} finally {
-				markupReader.close();
+				n++;
 			}
-			n++;
-			markupReader = getReader(MARKUP_FILE_NAME_TEMPLATE, n);
 		}
 		assertEquals(expectedNumberOfTestCases, n - 1);
 	}
 	
-	private Reader getReader(String template, int n) {
+	private String getContents(String template, int n) throws IOException {
 		String fileName = template.replace("{n}", Integer.toString(n));
-		InputStream in = getClass().getResourceAsStream(fileName);
-		if (in == null) {
+		URL url = getClass().getResource(fileName);
+		if (url == null) {
 			return null;
 		}
-		return new InputStreamReader(in);
+		return Resources.toString(url, Charsets.UTF_8);
 	}
 	
-	private String getContents(String template, int n) throws IOException {
-		Reader reader = getReader(template, n);
-		if (reader == null) {
-			return null;
-		}
-		
-		// TODO switch over to IOUtils.toString(Reader)
-		
-		BufferedReader bufferedReader = new BufferedReader(reader);
-		StringBuffer contents = new StringBuffer();
-		try {
-			String line = bufferedReader.readLine();
-			while (line != null) {
-				contents.append(line);
-				contents.append("\n");
-				line = bufferedReader.readLine();
-			}
-		} finally {
-			reader.close();
-		}
-		return contents.toString();
-	}
 }
